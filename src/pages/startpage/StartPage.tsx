7f6ade4f39a43fc624eb74f2ba84f7e5038c36a5
h4nsln3r <h4nsln3r@gmail.com>
2024-03-10 21:15:18 +0100

    Added some smoothness

--- src/pages/startpage/StartPage.tsx
+++ src/pages/startpage/StartPage.tsx
@@ -52,20 +52,37 @@ const StartPage = ({ CATEGORYS }: Props) => {
                         </div>
                     </aside>
                     <section className={`main-content ${targetCategory === "" ? '' : ''}`}>
-                        <div className={`init__header ${targetCategory === "" ? '' : 'init__header--fadeout'}`}>
-                            <div className={`init__header__bojeng ${targetCategory === "" ? '' : 'init__header--fadeout'}`} onClick={() => console.log("")}>
-                                BOJENG
+                        {targetCategory === "" && (
+                            <div className={`init__header ${targetCategory === "" ? '' : 'init__header--fadeout'}`}>
+                                <div className={`init__header__bojeng ${targetCategory === "" ? '' : 'init__header--fadeout'}`} onClick={() => console.log("")}>
+                                    BOJENG
+                                </div>
                             </div>
-                        </div>
+                        )}
 
                         <div className={`category Musik ${targetCategory === "Musik" ? 'category--active' : 'category-fadeout'}`} onClick={() => console.log("")}>
-                            <div className="category__section">{targetCategory}</div>
+                            <div className="category__section">
+                                {targetCategory}
+                                <p>youtube</p>
+                                <p>soundcloud</p>
+                            </div>
                         </div>
                         <div className={`category Spelningar ${targetCategory === "Spelningar" ? 'category--active' : 'category-fadeout'}`} onClick={() => console.log("")}>
-                            <div className="category__section">{targetCategory}</div>
+                            <div className="category__section">
+                                {targetCategory}
+                                <p>list</p>
+                            </div>
                         </div>
                         <div className={`category Om ${targetCategory === "Om" ? 'category--active' : 'category-fadeout'}`} onClick={() => console.log("")}>
-                            <div className="category__section">{targetCategory}</div>
+                            <div className="category__section">
+                                <p>Bild</p>
+                                {targetCategory}
+
+
+
+                                <p>Kontakt</p>
+
+                            </div>
                         </div>
 
                     </section>
