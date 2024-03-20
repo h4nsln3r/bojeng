05f4f349bd33c621cdfdd09084bf67f976b73eaf
h4nsln3r <h4nsln3r@gmail.com>
2024-03-20 18:19:04 +0100

    Update SocialMediaIcons.tsx

--- src/components/SocialMediaIcons/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.tsx
@@ -8,18 +8,26 @@ interface Props {
 const SocialMediaIcons = ({ className }: Props) => {
   return (
     <div className="icon-container">
-      <div className="icon-column">
+      <a href='https://soundcloud.com/bojengband?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
+        target='_blank'
+        className="icon-column">
         <FaSoundcloud className="icon" />
-      </div>
-      <div className="icon-column">
+      </a>
+      <a href='https://www.youtube.com/@bojengband'
+        target='_blank'
+        className="icon-column">
         <FaYoutube className="icon" />
-      </div>
-      <div className="icon-column">
+      </a>
+      <a href='https://www.instagram.com/bojengband'
+        target='_blank'
+        className="icon-column">
         <FaInstagram className="icon" />
-      </div>
-      <div className="icon-column">
+      </a>
+      <a href='https://www.facebook.com/bojengtheband'
+        target='_blank'
+        className="icon-column">
         <FaFacebook className="icon" />
-      </div>
+      </a>
     </div>
   );
 };
