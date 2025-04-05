f719696868d0649377d38d7b15e1ce3f49f2f3eb
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2025-04-05 23:02:29 +0200

    Cleaned up unused stuff

--- src/components/BackgroundImage/BackgroundImage.tsx
+++ src/components/BackgroundImage/BackgroundImage.tsx
@@ -1,19 +1,16 @@
 import './backgroundimage.scss';
 
 type Props = {
-    imageUrl: any;
-    showImage: boolean;
-}
+  imageUrl: any;
+  showImage: boolean;
+};
 
 const BackgroundImage = ({ imageUrl, showImage }: Props) => {
-
-
-
-    return (
-        <div className={`background-container ${showImage ? 'show' : ''}`}>
-            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
-        </div>
-    );
+  return (
+    <div className={`background-container ${showImage ? 'show' : ''}`}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    </div>
+  );
 };
 
 export default BackgroundImage;
