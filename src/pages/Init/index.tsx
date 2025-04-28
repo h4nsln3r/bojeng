e2c743380f57e618476ccc35db3ef22c46950252
h4nsln3r <h4nsln3r@gmail.com>
2025-04-28 20:38:30 +0200

    Done some new design
    with backgound image

--- src/pages/Init/index.tsx
+++ src/pages/Init/index.tsx
@@ -3,8 +3,6 @@ import ImageRotator from '../../components/ImageRotator/ImageRotator';
 import CountdownTimer from '../../components/CountdownTimer';
 import Footer from '../../components/Footer/Footer';
 
-import './init.scss';
-
 import BojengLogo from '../../assets/logo-text/bojeng_logotyp_white.png';
 import ZucchiniGif from '../../assets/zuc.gif';
 import Zucchini from '../Zucchini';
@@ -22,25 +20,27 @@ const Init = () => {
   };
 
   return (
-    <div className="container">
-      {!showZucchiniPage && showGif ? (
-        <div className="gif-overlay">
-          <img src={ZucchiniGif} alt="Intro GIF" className="intro-gif" />
-        </div>
-      ) : !showZucchiniPage ? (
-        <>
-          <ImageRotator intervalRange={[2000, 4000]} durationRange={[1000, 5000]} initialDelay={2000} />
+    <>
+      <div className="container">
+        {!showZucchiniPage && showGif ? (
+          <div className="gif-overlay">
+            <img src={ZucchiniGif} alt="Intro GIF" className="intro-gif" />
+          </div>
+        ) : !showZucchiniPage ? (
+          <>
+            <ImageRotator intervalRange={[2000, 4000]} durationRange={[1000, 5000]} initialDelay={2000} />
 
-          <main className="content content--center z-index--1">
-            <img className="bojeng__logo--large" src={BojengLogo} alt="Bojeng-Logo" />
-            <CountdownTimer targetDate="2025-06-06T00:00:00" onClickStart={handleCountdownClick} />
-          </main>
-        </>
-      ) : (
-        <Zucchini />
-      )}
+            <main className="start-content start-content--center z-index--1">
+              <img className="bojeng__logo--large" src={BojengLogo} alt="Bojeng-Logo" />
+              <CountdownTimer targetDate="2025-06-06T00:00:00" onClickStart={handleCountdownClick} />
+            </main>
+          </>
+        ) : (
+          <Zucchini />
+        )}
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
--- src/pages/Zucchini/index.tsx
+++ src/pages/Zucchini/index.tsx
@@ -2,36 +2,31 @@ import ImageRotator from '../../components/ImageRotator/ImageRotator';
 import Merch from '../../components/Merch';
 import Gigs from '../../components/Gigs';
 
-import BojengLogo from '../../assets/logo-text/bojeng_logotyp_white.png';
 import BojengAlbum from '../../assets/images/zucchini_bojeng.png';
+import Footer from '../../components/Footer/Footer';
 
 const Zucchini = () => {
   return (
     <div className="container">
-      <ImageRotator intervalRange={[2000, 4000]} durationRange={[1000, 5000]} initialDelay={6000} />
+      <ImageRotator intervalRange={[4000, 4000]} durationRange={[500, 1000]} initialDelay={4000} />
+      <img src={BojengAlbum} alt="Bojeng logo" className="background__bojeng-album" />
+      <main className="z-index--1">
+        <div className="flex--row">
+          <iframe
+            className="spotify-player"
+            src="https://open.spotify.com/embed/album/0xtOxVRRaA0kW7X4PlbtuP?utm_source=generator"
+            width="100%"
+            height="420px"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            title="Spotify Player"></iframe>
 
-      <main className="content content--center z-index--1">
-        <header>
-          <img className="bojeng__logo" src={BojengLogo} alt="Bojeng-Logo" />
-        </header>
-        <div className="flex--3 max-width--800 margin__bottom--4">
-          <section className="section__spotify">
-            <iframe
-              src="https://open.spotify.com/embed/album/0xtOxVRRaA0kW7X4PlbtuP?utm_source=generator"
-              width="100%"
-              height="420px"
-              frameBorder="0"
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-              title="Spotify Player"></iframe>
-          </section>
-          <div className="">
-            <img src={BojengAlbum} alt="Bojeng logo" className="bojeng__album" />
-          </div>
           <Gigs />
         </div>
         <Merch />
       </main>
+      <Footer /> {/* Footer kommer alltid sist */}
     </div>
   );
 };
