f7a5675f955a0546c23f3234ae2b7e56923132f4
h4nsln3r <h4nsln3r@gmail.com>
2025-05-16 20:19:29 +0200

    Update index.tsx

--- src/pages/Init/index.tsx
+++ src/pages/Init/index.tsx
@@ -37,9 +37,9 @@ const Init = () => {
             <main className="start-content start-content--center z-index--1">
               <img className="bojeng__logo--large" src={BojengLogo} alt="Bojeng-Logo" />
               <CountdownTimer targetDate="2025-05-25T00:00:00" onClickStart={handleCountdownClick} />
-              {/* <a href="https://share.amuse.io/track/bojeng-misery" className="presave__button" rel="noreferrer" target="_blank">
+              <a href="https://share.amuse.io/track/bojeng-misery" className="presave__button" rel="noreferrer" target="_blank">
                 Presave
-              </a> */}
+              </a>
             </main>
           </>
         ) : (
