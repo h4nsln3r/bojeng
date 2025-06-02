29361236490c4608339571fd8a01b18dec4d6446
h4nsln3r <h4nsln3r@gmail.com>
2025-06-02 17:50:34 +0200

    More footer cleanup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,14 @@ import { FaFacebook, FaInstagram, FaSoundcloud, FaYoutube, FaSpotify } from 'rea
 import './footer.scss';
 import Icon from '../Icon';
 
+const LINKS = {
+  Soundcloud: 'https://soundcloud.com/bojengband?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing',
+  Youtube: 'https://www.youtube.com/@bojengband',
+  Instagram: 'https://www.instagram.com/bojengband',
+  Facebook: 'https://www.facebook.com/bojengband',
+  Spotify: 'https://open.spotify.com/artist/1BFiTHDwBWhaxVJ7JDi0fh?si=lf7AvMFTSg6xLO4ZO0fR-w',
+};
+
 const Footer = () => {
   return (
     <footer className="footer">
@@ -9,24 +17,13 @@ const Footer = () => {
         <div className="contact">CONTACT: BOJENGTHEBAND@GMAIL.COM</div>
       </div>
       <div className="footer__column footer__column--center footer__icons">
-        <Icon
-          link={'https://soundcloud.com/bojengband?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'}
-          icon={<FaSoundcloud className="icon" />}
-        />
-        <Icon link={'https://www.youtube.com/@bojengband'} icon={<FaYoutube className="icon" />} />
-        <Icon link={'https://www.instagram.com/bojengband'} icon={<FaInstagram className="icon" />} />
-        <Icon link={'https://www.facebook.com/bojengband'} icon={<FaFacebook className="icon" />} />
-        <Icon link={'https://open.spotify.com/artist/1BFiTHDwBWhaxVJ7JDi0fh?si=lf7AvMFTSg6xLO4ZO0fR-w'} icon={<FaSpotify className="icon" />} />
+        <Icon link={LINKS.Soundcloud} icon={<FaSoundcloud className="icon" />} />
+        <Icon link={LINKS.Youtube} icon={<FaYoutube className="icon" />} />
+        <Icon link={LINKS.Instagram} icon={<FaInstagram className="icon" />} />
+        <Icon link={LINKS.Facebook} icon={<FaFacebook className="icon" />} />
+        <Icon link={LINKS.Spotify} icon={<FaSpotify className="icon" />} />
       </div>
-
-      <div className="footer__column footer__column--right">
-        {/* <div className="foto">
-          Foto:{' '}
-          <a href="https://www.instagram.com/urbanan/" target="true">
-            Urban Anjar
-          </a>
-        </div> */}
-      </div>
+      <div className="footer__column footer__column--right"></div>
     </footer>
   );
 };
