dfddf70ece8003c69c27545d34f69494ea6656fe
h4nsln3r <h4nsln3r@gmail.com>
2025-11-07 23:26:37 +0100

    Start on hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './hero.scss';
+import BojengLogo from '../../assets/logo-text/bojeng_logotyp_white.png';
 
 const Hero = () => {
   return (
     <section className="hero" aria-label="Bojeng hero" id="top">
       <div className="hero__bg" />
+
       <div className="container hero__inner">
-        <h1 className="hero__title">Bojeng</h1>
-        <p className="hero__subtitle">Ny musik ute nu</p>
+        <h1 className="hero__title">
+          <img src={BojengLogo} />
+        </h1>
+        {/* <p className="hero__subtitle">Ny musik ute nu</p> */}
         <div className="hero__cta">
           <a className="btn btn--solid" href="#listen">
             Lyssna
