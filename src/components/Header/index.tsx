a0ebe9ab2e84f82b3f84f0f00b1790dbc0b64d17
h4nsln3r <h4nsln3r@gmail.com>
2025-11-08 12:59:36 +0100

    Added onlyzuc to page nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 import './header.scss';
 import Navigation from '../Navigation';
+import Zucchini from '../../assets/icons/onlyzuc.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container header__inner">
         <a href="#top" className="brand" aria-label="Bojeng – startsida">
-          <span className="brand__logo" aria-hidden="true">
+          <img className="brand__logo" src={Zucchini} />
+          {/* <span className="brand__logo" aria-hidden="true">
             ●
-          </span>
-          <span className="brand__text">Bojeng</span>
+          </span> */}
+          {/* <span className="brand__text">Bojeng</span> */}
         </a>
         <Navigation />
       </div>
