e1c841d11bcb8ff399c2c1855b4137a9958ed50b
h4nsln3r <h4nsln3r@gmail.com>
2025-11-19 21:05:53 +0100

    Added lightwidget instagram feed

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,16 +1,31 @@
+import { useEffect } from 'react';
 import './_sections.scss';
+// import BojengImage from '../../assets/photos/DSC03701-3.jpg';
 
 const SOCIALS = [
-  { name: 'Instagram', href: 'https://instagram.com/bojeng', aria: 'Instagram' },
+  { name: 'Instagram', href: 'https://instagram.com/bojengband', aria: 'Instagram' },
   { name: 'Spotify', href: 'https://open.spotify.com/artist/XXX', aria: 'Spotify' },
   { name: 'YouTube', href: 'https://youtube.com/@bojeng', aria: 'YouTube' },
   { name: 'TikTok', href: 'https://tiktok.com/@bojeng', aria: 'TikTok' },
 ];
 
 const Contact = () => {
+  // Laddar LightWidget-skriptet korrekt i React
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.lightwidget.com/widgets/lightwidget.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="container">
       <h2 className="section__title">Kontakt</h2>
+
       <div className="contact-card">
         <p>
           Bokning/press:{' '}
@@ -18,6 +33,7 @@ const Contact = () => {
             booking@bojeng.com
           </a>
         </p>
+
         <ul className="socials">
           {SOCIALS.map((s) => (
             <li key={s.name}>
@@ -28,8 +44,19 @@ const Contact = () => {
           ))}
         </ul>
       </div>
-      <br />
-      <br />
+
+      {/* --- INSTAGRAM FEED --- */}
+      <div className="instagram-feed">
+        <h3 className="section__subtitle">Senaste från Instagram</h3>
+
+        <iframe
+          src="//lightwidget.com/widgets/ad42fc24466952b499b263d00fb8d9be.html"
+          scrolling="no"
+          // allowTransparency="true"
+          className="lightwidget-widget instagram-embed"
+          style={{ width: '100%', border: 0, overflow: 'hidden' }}
+        ></iframe>
+      </div>
     </div>
   );
 };
--- src/components/Sections/Listen.tsx
+++ src/components/Sections/Listen.tsx
@@ -4,9 +4,10 @@ import './_sections.scss';
 // === Dummy data – byt till riktiga länkar/filer ===
 const LATEST_SINGLE = {
   title: 'Ny singel: ”Ljus över Bojeng”',
-  spotify: 'https://open.spotify.com/embed/track/XXXXXXXXXXXX', // byt
-  apple: 'https://embed.music.apple.com/se/album/XXXXXXXXXXXX', // byt
-  soundcloud: 'https://w.soundcloud.com/player/?url=XXXXXXXXXX', // byt
+  youtube: 'https://youtube.com/shorts/q4LA_MZsQ9Y?si=rzkqg9ourTyQM9rW', // byt
+  apple: 'https://embed.music.apple.com/se/album/bojeng-ep/1814354097', // byt
+  soundcloud:
+    'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2151444258&color=%23135c19&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true', // byt
 };
 const Listen = () => {
   return (
@@ -20,23 +21,25 @@ const Listen = () => {
       </div>
       <br />
       <div className="players">
-        {LATEST_SINGLE.spotify && (
+        {LATEST_SINGLE.youtube && (
           <div className="player-card">
-            <h3>Spotify</h3>
             <div className="ratio">
               <iframe
-                title="Spotify"
-                src={LATEST_SINGLE.spotify}
-                loading="lazy"
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              />
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/xCPSnubQclw?si=aYa9f0QAI7H_m0sS"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         )}
 
         {LATEST_SINGLE.apple && (
           <div className="player-card">
-            <h3>Apple Music</h3>
             <div className="ratio">
               <iframe title="Apple Music" src={LATEST_SINGLE.apple} loading="lazy" />
             </div>
@@ -45,9 +48,13 @@ const Listen = () => {
 
         {LATEST_SINGLE.soundcloud && (
           <div className="player-card">
-            <h3>SoundCloud</h3>
             <div className="ratio">
-              <iframe title="SoundCloud" src={LATEST_SINGLE.soundcloud} loading="lazy" />
+              <iframe
+                className="player-card--souncloud"
+                title="SoundCloud"
+                src={LATEST_SINGLE.soundcloud}
+                loading="lazy"
+              />
             </div>
           </div>
         )}
