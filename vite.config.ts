3421db77d126bb6e6ffce2d0958d029f6364da20
h4nsln3r <h4nsln3r@gmail.com>
2025-12-13 18:30:02 +0100

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/bojeng/', // 👈 viktigt för GitHub Pages /bojeng/
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   server: { port: 5173, open: true },
   build: { sourcemap: true },
