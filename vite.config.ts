7a0d95a8faf3a938d9a4834a5a5824381685723a
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2026-01-13 19:15:35 +0100

    Updated sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/',
   plugins: [react(), tsconfigPaths()],
   server: { port: 5173, open: true },
-  build: { sourcemap: true },
-});
+  build: {
+    sourcemap: mode !== 'production', // ← av i prod
+  },
+}));
