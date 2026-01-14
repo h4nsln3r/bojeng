8247f5f534b3951ee0b420d5eb15113d215c811f
h4nsln3r <h4nsln3r@gmail.com>
2026-01-14 09:10:30 +0100

    Build and deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: './',
   plugins: [react(), tsconfigPaths()],
   server: { port: 5173, open: true },
   build: {
